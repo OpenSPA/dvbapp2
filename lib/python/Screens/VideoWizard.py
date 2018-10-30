6b7491b66b6af3f59c8372e619995c01c9ebf732
openspa <openspateam@gmail.com>
2018-10-30 13:56:38 +0100

    avswitch cleanup scart and yuv detection

--- lib/python/Screens/VideoWizard.py
+++ lib/python/Screens/VideoWizard.py
@@ -16,10 +16,12 @@ config.misc.showtestcard = ConfigBoolean(default = False)
 has_rca = False
 has_dvi = False
 has_jack = False
+has_scart = False
 
 has_rca = SystemInfo["HaveRCA"]
 has_dvi = SystemInfo["HaveDVI"]
 has_jack = SystemInfo["HaveAVJACK"]
+has_scart = SystemInfo["HAVESCART"]
 
 class VideoWizardSummary(WizardSummary):
 	skin = (
@@ -103,9 +105,9 @@ class VideoWizard(WizardLanguage, Rc):
 				descr = port
 				if descr == 'HDMI' and has_dvi:
 					descr = 'DVI'
-				if descr == 'Scart' and has_rca:
+				if descr == 'Scart' and has_rca and not has_scart:
 					descr = 'RCA'
-				if descr == 'Scart' and has_jack:
+				if descr == 'Scart' and has_jack and not has_scart:
 					descr = 'Jack'
 				if port != "DVI-PC":
 					list.append((descr,port))
