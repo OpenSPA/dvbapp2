cec782d74a728b63d718d5f1755d7c2f4acb3d68
openspa <openspateam@gmail.com>
2019-01-10 17:53:41 +0100

    add u55

--- lib/python/Plugins/SystemPlugins/SoftwareManager/Flash_online.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/Flash_online.py
@@ -870,7 +870,7 @@ class doFlashImage(Screen):
 						cmdlist.append("%s -n -r -k -m%s %s > /dev/null 2>&1" % (ofgwritePath, self.multi, self.flashTmp))
 				elif getMachineBuild() in ("h9combo", "u51", "u52", "u53", "u54", "u5", "u5pvr", "cc1", "sf8008","sf8008s", "ustym4kpro", "hd60", "hd61", "v8plus"):
 					cmdlist.append("%s -n -r%s -k%s %s > /dev/null 2>&1" % (ofgwritePath, MTDROOTFS, MTDKERNEL, self.flashTmp))
-				elif getMachineBuild() in ("h9", "i55plus"):
+				elif getMachineBuild() in ("h9", "i55plus", "u55"):
 					cmdlist.append("%s -n -f -r -k %s > /dev/null 2>&1" % (ofgwritePath, self.flashTmp))
 				else:
 					cmdlist.append("%s -n -r -k %s > /dev/null 2>&1" % (ofgwritePath, self.flashTmp))
@@ -889,7 +889,7 @@ class doFlashImage(Screen):
 						cmdlist.append("%s -r -k -m%s %s > /dev/null 2>&1" % (ofgwritePath, self.multi, self.flashTmp))
 				elif getMachineBuild() in ("h9combo", "u51", "u52", "u53", "u54", "u5", "u5pvr", "cc1", "sf8008","sf8008s", "ustym4kpro", "hd60", "hd61", "v8plus"):
 					cmdlist.append("%s -r%s -k%s %s > /dev/null 2>&1" % (ofgwritePath, MTDROOTFS, MTDKERNEL, self.flashTmp))
-				elif getMachineBuild() in ("h9", "i55plus"):
+				elif getMachineBuild() in ("h9", "i55plus", "u55"):
 					cmdlist.append("%s -f -r -k %s > /dev/null 2>&1" % (ofgwritePath, self.flashTmp))
 				else:
 					cmdlist.append("%s -r -k %s > /dev/null 2>&1" % (ofgwritePath, self.flashTmp))
