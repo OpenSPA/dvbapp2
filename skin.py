c1781973e83f665ba852e339bd7fafd3c41b8d47
viptronick@gmail.com <viptronick@gmail.com>
2021-07-07 17:49:17 +0200

    [skin.py]add default skin

--- skin.py
+++ skin.py
@@ -14,7 +14,7 @@ from Tools.Directories import SCOPE_CONFIG, SCOPE_CURRENT_LCDSKIN, SCOPE_CURRENT
 from Tools.Import import my_import
 from Tools.LoadPixmap import LoadPixmap
 
-DEFAULT_SKIN = "Spa24HD/skin.xml"
+DEFAULT_SKIN = "OpenStarHD/skin.xml"
 EMERGENCY_SKIN = "skin_default.xml"
 EMERGENCY_NAME = "Default OE-A"
 DEFAULT_DISPLAY_SKIN = "skin_display_grautec.xml" if SystemInfo["grautec"] else "skin_display.xml"
