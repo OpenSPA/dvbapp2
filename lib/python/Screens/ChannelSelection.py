22e76922c94515e9fe431dd4b5d29496d5473911
Captain <openatv@gmail.com>
2024-07-18 02:59:02 +0200

    [ChannelSelection] fix typo RdsAction

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -3024,7 +3024,7 @@ class ChannelSelectionRadio(ChannelSelectionBase, ChannelSelectionEdit, ChannelS
 		self["RdsActions"] = HelpableActionMap(self, ["InfobarRdsActions"], {
 			"startRassInteractive": (self.startRassInteractive, _("View Rass interactive"))
 		}, prio=-1, description=_("Radio Channel Selection Actions"))
-		self["rdsActions"].setEnabled(False)
+		self["RdsActions"].setEnabled(False)
 		infobar.rds_display.onRassInteractivePossibilityChanged.append(self.RassInteractivePossibilityChanged)
 		self.onClose.append(self.__onClose)
 		self.onExecBegin.append(self.__onExecBegin)
@@ -3047,7 +3047,7 @@ class ChannelSelectionRadio(ChannelSelectionBase, ChannelSelectionEdit, ChannelS
 		self.infobar.RassSlidePicChanged()
 
 	def RassInteractivePossibilityChanged(self, state):
-		self["rdsActions"].setEnabled(state)
+		self["RdsActions"].setEnabled(state)
 
 	def __onExecBegin(self):
 		self.info.show()
