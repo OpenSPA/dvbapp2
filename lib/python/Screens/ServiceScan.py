50c59ac6600034dc26acfe95e174f156d210572b
norhap <elfogondesegovia@gmail.com>
2024-10-12 22:02:17 +0200

    ServiceScan.py: add FIFOList class

--- lib/python/Screens/ServiceScan.py
+++ lib/python/Screens/ServiceScan.py
@@ -22,6 +22,24 @@ from Tools.Transponder import getChannelNumber
 MODULE_NAME = __name__.split(".")[-1]
 
 
+class FIFOList(MenuList):
+	def __init__(self, len=10):
+		self.len = len
+		self.list = []
+		MenuList.__init__(self, self.list)
+	def addItem(self, item):
+		self.list.append(item)
+		self.l.setList(self.list[-self.len:])
+	def clear(self):
+		del self.list[:]
+		self.l.setList(self.list)
+	def getCurrentSelection(self):
+		return self.list and self.getCurrent() or None
+	def listAll(self):
+		self.l.setList(self.list)
+		self.selectionEnabled(True)
+
+
 class ServiceScan(Screen):
 	RUNNING = 0
 	DONE = 1
