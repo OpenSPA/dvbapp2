cd35ed1166745b3128435d0b08c046691689d87a
norhap <elfogondesegovia@gmail.com>
2024-10-12 23:06:54 +0200

    ServiceScan.py: PEP8

--- lib/python/Screens/ServiceScan.py
+++ lib/python/Screens/ServiceScan.py
@@ -27,14 +27,18 @@ class FIFOList(MenuList):
 		self.len = len
 		self.list = []
 		MenuList.__init__(self, self.list)
+
 	def addItem(self, item):
 		self.list.append(item)
 		self.l.setList(self.list[-self.len:])
+
 	def clear(self):
 		del self.list[:]
 		self.l.setList(self.list)
+
 	def getCurrentSelection(self):
 		return self.list and self.getCurrent() or None
+
 	def listAll(self):
 		self.l.setList(self.list)
 		self.selectionEnabled(True)
