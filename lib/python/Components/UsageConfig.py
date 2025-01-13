fc3de3306018f4276d56ab8d9ec14f619a1b789b
jbleyel <jbleyel@gmx.net>
2025-01-13 12:06:02 +0100

    [Timeshift] * fix timeshift recording path calculation

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -2522,6 +2522,10 @@ def preferredInstantRecordPath():
 	return preferredPath(config.usage.instantrec_path.value)
 
 
+def preferredTimeShiftRecordingPath():
+	return preferredPath(config.timeshift.recordingPath.value) or defaultMoviePath()
+
+
 def defaultMoviePath():
 	return defaultRecordingLocation(config.usage.default_path.value)
 
